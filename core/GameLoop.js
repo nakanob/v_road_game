9b32e0e7853a62052ca83b9c3d42ad6dddcc39db
nakanob <babavavasts@gmail.com>
2026-07-01 12:29:49 +0900

    Create GameLoop.js

---
+++ core/GameLoop.js
@@ -0,0 +1,51 @@
+export class GameLoop {
+
+    constructor(sceneManager) {
+
+        this.sceneManager = sceneManager;
+
+        this.running = false;
+
+        this.animationId = null;
+
+        this.animate = this.animate.bind(this);
+
+    }
+
+    start() {
+
+        if (this.running) return;
+
+        this.running = true;
+
+        this.animate();
+
+    }
+
+    stop() {
+
+        this.running = false;
+
+        if (this.animationId !== null) {
+
+            cancelAnimationFrame(this.animationId);
+
+            this.animationId = null;
+
+        }
+
+    }
+
+    animate() {
+
+        if (!this.running) return;
+
+        this.animationId = requestAnimationFrame(this.animate);
+
+        this.sceneManager.update();
+
+        this.sceneManager.render();
+
+    }
+
+}
