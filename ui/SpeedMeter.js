1c84d948fc49152145258116740e4e5776997b02
nakanob <babavavasts@gmail.com>
2026-07-02 10:32:32 +0900

    Create SpeedMeter.js

---
+++ ui/SpeedMeter.js
@@ -0,0 +1,40 @@
+// ui/SpeedMeter.js
+
+export class SpeedMeter {
+
+    constructor(sceneManager, vehicle) {
+
+        this.sceneManager = sceneManager;
+        this.vehicle = vehicle;
+
+        this.container = document.createElement("div");
+        this.container.id = "speedometer";
+
+        this.value = document.createElement("div");
+        this.value.id = "speedometer-value";
+
+        this.unit = document.createElement("div");
+        this.unit.id = "speedometer-unit";
+
+        this.value.textContent = "0";
+        this.unit.textContent = "km/h";
+
+        this.container.appendChild(this.value);
+        this.container.appendChild(this.unit);
+
+        document.body.appendChild(this.container);
+
+    }
+
+    update() {
+
+        const speed = Math.abs(
+            this.vehicle.speed * 3.6
+        );
+
+        this.value.textContent =
+            Math.round(speed);
+
+    }
+
+}
