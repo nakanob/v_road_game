fe6271f0ef183dc4105ee03788dc5fe53df8235f
nakanob <babavavasts@gmail.com>
2026-07-06 10:30:22 +0900

    Update Vehicle.js

--- vehicle/Vehicle.js
+++ vehicle/Vehicle.js
@@ -77,27 +77,7 @@ export class Vehicle {
         this.headLights = [];
         this.tailLights = [];
 
-        updateLights() {
 
-            const isNight =
-                this.sceneManager.sun
-                    ? this.sceneManager.sun.isNight
-                    : false;
-        
-            for (const light of this.headLights) {
-        
-                light.intensity =
-                    isNight ? 35 : 0;
-        
-            }
-        
-            for (const lamp of this.tailLights) {
-        
-                lamp.visible = isNight;
-        
-            }
-        
-        }
     }
 
     async load() {
